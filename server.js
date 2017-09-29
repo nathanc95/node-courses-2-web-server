34c41b8a204a01ce2986ebf1d54cb72c77858e40
nathan <nathanchemla95@gmail.com>
2017-09-29 11:34:50 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+var app=express();
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+  var now= new Date().toString();
+  var log= `${now}: ${req.method} ${req.url}}`;
+  console.log(log);
+  fs.appendFile('server.log',log + '\n',(err)=>{
+    if(err){
+      console.log('unable to append');
+    }
+  });
+  next();
+});
+
+app.use((req,res,next)=>{
+  req.get='/';
+  res.render('maintenance.hbs',{
+
+  });
+  next();
+});
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+
+app.get('/',(req,res)=>{
+  //res.send('hello express');
+  res.render('index.hbs',{
+    pageTitle:'Home Page page',
+    welcomeUser:'hello you'
+  });
+});
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:'About page',
+  });
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    request:'this is a bad request'
+  });
+});
+app.listen(3000);
